14e5defc8065be8477b47030857c920715a34b67
Davor Juric <davorxxjuric@gmail.com>
2023-11-26 13:58:48 -0500

    Initial Smart Contract commit

---
+++ test/integration/InteractionsTest.t.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../../src/FundMe.sol";
+import {DeployFundMe} from "../../script/DeployFundMe.s.sol";
+import {FundFundMe, WithdrawFundMe} from "../../script/Interactions.s.sol";
+
+contract InteractionsTest is Test {
+    FundMe fundMe;
+
+    //address public constant USER = address(1);
+    address USER = makeAddr("user");
+    uint256 constant SEND_VALUE = 0.1 ether;
+    uint256 constant STARTING_BALANCE = 10 ether;
+    uint256 constant GAS_PRICE = 1;
+
+    function setUp() external {
+        DeployFundMe deploy = new DeployFundMe();
+        fundMe = deploy.run();
+        vm.deal(USER, STARTING_BALANCE);
+    }
+
+    function testUserUserCanFundInteractions() public {
+        FundFundMe fundFundMe = new FundFundMe();
+        // vm.prank(USER);
+        // vm.deal(USER, 1e18);
+        fundFundMe.fundFundMe(address(fundMe));
+
+        address funder = fundMe.getFunder(0);
+        assertEq(funder, msg.sender);
+    }
+
+    function testUserUserCaWithdrawInteractions() public {
+        FundFundMe fundFundMe = new FundFundMe();
+        fundFundMe.fundFundMe(address(fundMe));
+
+        WithdrawFundMe withdrawFundMe = new WithdrawFundMe();
+        withdrawFundMe.withdrawFundMe(address(fundMe));
+        //address funder = fundMe.getFunder(0);
+        assert(address(fundMe).balance == 0);
+    }
+}
